81a03924572199c408a070de89e768d4bdb06aae
Boluwatife Adediwura <adediwuraboluwatife1@gmail.com>
2023-09-19 13:44:31 +0100

    updated tire tests

--- tests/test_tire/test_carrigan.py
+++ tests/test_tire/test_carrigan.py
@@ -4,12 +4,12 @@ from tire.carriganTire import CarriganTire
 
 
 class TestCarriganTires(unittest.TestCase):
-    def test_needs_service_true(self):
-        tire_wear = [0.1, 0.3, 0.2, 0.9]
+    def does_needs_service(self):
+        tire_wear = [0.2, 0.2, 0.5, 0.4]
         tires = CarriganTire(tire_wear)
         self.assertTrue(tires.needs_service())
 
-    def test_needs_service_false(self):
-        tire_wear = [0.1, 0.2, 0.4, 0.2]
+    def doesnot_needs_service(self):
+        tire_wear = [0.1, 0.1, 0.3, 0.3]
         tires = CarriganTire(tire_wear)
         self.assertFalse(tires.needs_service())
--- tests/test_tire/test_octoprime.py
+++ tests/test_tire/test_octoprime.py
@@ -4,12 +4,12 @@ from tire.octoprimeTire import OctoprimeTire
 
 
 class TestCarriganTires(unittest.TestCase):
-    def test_needs_service_true(self):
-        tire_wear = [0.1, 0.3, 0.2, 0.9]
+    def does_needs_service(self):
+        tire_wear = [0.2, 0.1, 0.6, 0.3]
         tires = OctoprimeTire(tire_wear)
         self.assertTrue(tires.needs_service())
 
-    def test_needs_service_false(self):
-        tire_wear = [0.1, 0.2, 0.4, 0.2]
+    def doesnot_needs_service(self):
+        tire_wear = [0.1, 0.2, 0.3, 0.3]
         tires = OctoprimeTire(tire_wear)
         self.assertFalse(tires.needs_service())
